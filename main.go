7195cdac69b7ee0a272573700c2fc1cc5955c9c8
Javier Ribal del Rio <javierribal@gmail.com>
2026-02-07 23:34:13 +0100

    feat(main): create interfaces

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Hyperloop-UPV/NATSOS/pkg/adj"
 	"github.com/Hyperloop-UPV/NATSOS/pkg/config"
+	"github.com/Hyperloop-UPV/NATSOS/pkg/network"
 )
 
 func main() {
@@ -20,10 +21,25 @@ func main() {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
+	// Set up the network configuration
+	if err := network.SetupNetwork(); err != nil {
+		log.Fatalf("Failed to setup network: %v", err)
+	}
+
+	err = network.SetupExternalInterface(cfg.Network.Interface, "192.168.0.1/16")
+	if err != nil {
+		log.Fatalf("Failed to setup external interface: %v", err)
+	}
 	// get the ADJ branch from the configuration and print it
 	adj, err := adj.NewADJ(cfg.ADJBranch, cfg.ADJPath)
 	if err != nil {
 		log.Fatalf("Failed to initialize ADJ: %v at %s", err, cfg.ADJPath)
+	}
+
+	// create a dummy interface for each board in the ADJ
+	for _, board := range adj.Boards {
+
+		network.SetupDummyInterface(board.Name, board.IP)
 	}
 
 	// Print the ADJ info and boards
