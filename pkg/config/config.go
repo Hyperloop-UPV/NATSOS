170d7555055176b9007a4857d5b4f661392cff83
Javier Ribal del Rio <javierribal@gmail.com>
2026-02-08 17:54:31 +0100

    fix(config): add initial period-ms

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,9 +10,10 @@ import (
 // Extend this struct with your specific configuration fields.
 type Config struct {
 	// Application settings
-	ADJBranch string  `json:"adj-branch"`
-	ADJPath   string  `json:"adj-path"` // Path to the ADJ repository empty for download usingg git
-	Network   Network `json:"network"`
+	ADJBranch     string  `json:"adj-branch"`
+	ADJPath       string  `json:"adj-path"` // Path to the ADJ repository empty for download usingg git
+	Network       Network `json:"network"`
+	InitialPeriod uint64  `json:"initial-period"` // Initial period for packet generation in milliseconds
 }
 
 type Network struct {
